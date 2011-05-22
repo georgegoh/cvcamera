e8487bf02dcfc766be99dae4015571b398f2c3d2
George Goh <georgegohkokleong@gmail.com>
2011-05-22 17:12:42 +0800

    Fine tuning GFTT detector.

--- jni/Processor.cpp
+++ jni/Processor.cpp
@@ -15,7 +15,7 @@ Processor::Processor() :
       stard(20/*max_size*/, 8/*response_threshold*/, 15/*line_threshold_projected*/, 8/*line_threshold_binarized*/, 5/*suppress_nonmax_size*/),
       fastd(20/*threshold*/, true/*nonmax_suppression*/),
       surfd(100./*hessian_threshold*/, 1/*octaves*/, 2/*octave_layers*/),
-      gfttd(6/*max_corners*/, 0.20/*quality_level*/, 5/*min_distance*/, 5/*block_size*/, false/*use_harris_detector*/, 0.2/*k*/)
+      gfttd(100/*max_corners*/, 0.01/*quality_level*/, 5.0/*min_distance*/, 3/*block_size*/, true/*use_harris_detector*/, 0.04/*k*/)
 {
 
 }
